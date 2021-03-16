b72a3d016ca4d02b15655716fdef94b08bd6e026
thepattj <jdej261@gmail.com>
2021-03-15 23:38:49 -0600

    index
    se realizo la  primer pagina del cambio... faltan 18 + 1

--- index.php
+++ index.php
@@ -51,6 +51,8 @@
     <!-- modernizr JS
 		============================================ -->
     <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+    <!-- archivos utilizados -->
+    <link rel="stylesheet" type="text/css" href="css/estilos.css">
 </head>
 
 <body>
@@ -58,36 +60,43 @@
             <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
     <!-- Login Register area Start-->
+
+    
     <div class="login-content">
-        <!-- Login -->
         <div class="nk-block toggled" id="l-login">
             <div class="nk-form">
                 <div class="input-group">
-                    <span class="input-group-addon nk-ic-st-pro"><i class="notika-icon notika-support"></i></span>
+                    <span class="input-group-addon nk-ic-st-pro" style="background: rgb(27,22,50);"><!-- <i class="notika-icon notika-support"></i> --></span>
                     <div class="nk-int-st">
-                        <input type="text" class="form-control" id="user" placeholder="Usuario">
+                        <input type="text" class="form-control" style="background: rgb(27,22,50);" id="user" placeholder="Usuario">
                     </div>
                 </div>
                 <div class="input-group mg-t-15">
-                    <span class="input-group-addon nk-ic-st-pro"><i class="notika-icon notika-edit"></i></span>
+                    <span class="input-group-addon nk-ic-st-pro" style="background: rgb(27,22,50);"><!-- <i class="notika-icon notika-edit"></i> --></span>
                     <div class="nk-int-st">
-                        <input type="password" class="form-control" id="pass" placeholder="Contraseña">
+                        <input type="password" class="form-control" style="background: rgb(27,22,50);" id="pass" placeholder="Contraseña">
                     </div>
                 </div>
-                <!-- <div class="fm-checkbox">
-                    <label><input type="checkbox" class="i-checks"> <i></i> Recordarme</label>
-                </div> -->
-                <a href="" data-ma-action="" data-ma-block="" class="btn btn-login btn-success btn-float" id="btnInicio">
+                <div class="fm-checkbox">
+                    <label style="color: white;"><input type="checkbox" class="i-checks"> <i></i> Recordarme</label>
+
+                    <button id="btnInicio" style=" width:35%; background: rgb(254,174,0); color: rgb(27,22,50); margin-left: 20%; border: none; border-radius: 5px;">Iniciar</button>
+                </div>
+                <div>
+                    
+                </div>
+                <!-- <a href="" data-ma-action="" data-ma-block="" class="btn btn-login btn-success btn-float" >
                     <i class="notika-icon notika-right-arrow right-arrow-ant"></i>
-                </a>
+                </a> -->
             </div>
 
-            <!-- <div class="nk-navigation nk-lg-ic">
+            <!-- SE BORRA EL RECUPERADOR DE CONTRASEÑA -->
+             <!-- <div class="nk-navigation nk-lg-ic">
                 <a href="#" data-ma-action="nk-login-switch" data-ma-block="#l-forget-password"><i>?</i> <span>Recupera contraseña</span></a>
-            </div> -->
+            </div>  -->
         </div>
 
-        <!-- Register -->
+        <!--  Register
         <div class="nk-block" id="l-register">
             <div class="nk-form">
                 <div class="input-group">
@@ -112,16 +121,17 @@
                 </div>
 
                 <a href="#l-login" data-ma-action="nk-login-switch" data-ma-block="#l-login" class="btn btn-login btn-success btn-float"><i class="notika-icon notika-right-arrow"></i></a>
-            </div>
+            </div> -->
 
-            <div class="nk-navigation rg-ic-stl">
+            <!-- SE BORRRAN LOS BOTONES DE RECUPERACION E INGRESO -->
+            <!-- <div class="nk-navigation rg-ic-stl">
                 <a href="#" data-ma-action="nk-login-switch" data-ma-block="#l-login"><i class="notika-icon notika-right-arrow"></i> <span>Sign in</span></a>
                 <a href="" data-ma-action="nk-login-switch" data-ma-block="#l-forget-password"><i>?</i> <span>Forgot Password</span></a>
-            </div>
+            </div> -->
         </div>
 
-        <!-- Forgot Password -->
-        <div class="nk-block" id="l-forget-password">
+         <!-- Forgot Password  -->
+        <!-- <div class="nk-block" id="l-forget-password">
             <div class="nk-form">
                 <p class="text-left">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eu risus. Curabitur commodo lorem fringilla enim feugiat commodo sed ac lacus.</p>
 
@@ -139,8 +149,10 @@
                 <a href="" data-ma-action="nk-login-switch" data-ma-block="#l-login"><i class="notika-icon notika-right-arrow"></i> <span>Sign in</span></a>
                 <a href="" data-ma-action="nk-login-switch" data-ma-block="#l-register"><i class="notika-icon notika-plus-symbol"></i> <span>Register</span></a>
             </div>
-        </div>
+        </div> -->
     </div>
+
+    
     <!-- Login Register area End-->
     <!-- jquery
 		============================================ -->
